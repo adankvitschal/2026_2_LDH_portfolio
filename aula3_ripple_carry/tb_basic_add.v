cbeb3c785bbb15c7456f5ca3ae611757c28ce472
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-09-01 10:39:03 -0300

    Somador ripple carry com testbench basico Arquivos Verilog das instâncias full_adder e adder para um somador em topologia ripple carry, implementado em modelagem dataflow. Arquivos correspondem à aula 3.
    Signed-off-by: Adan Kvitschal <adan.kvitschal@ifsc.edu.br>

---
+++ aula3_ripple_carry/tb_basic_add.v
@@ -0,0 +1,28 @@
+`timescale 1us/1us
+
+module tb_basic_add;
+
+	//Sinais
+	reg 	[3:0]	a;
+	reg 	[3:0]	b;
+	wire 	[4:0]	s;
+	
+	//Instância do DUT
+	adder_4b dut (
+		.a(a), .b(b), .s(s)
+	);
+	
+	//Stimuli
+	initial begin //Bloco initial, executa uma única vez (não é sintetizável)
+		a = 0; b = 0;
+		#10
+		a = 1; b = 1;
+		#10
+		a = 3; b = 5;
+		#10
+		a = 7; b = 9;
+		#10
+		$finish;
+	end
+	
+endmodule
